a4b782fc2d92086f6945943658d52e051a7ad734
ManojAnnam <annam.manoj1996@gmail.com>
2020-03-16 08:43:09 +0530

    Added Scripts File

---
+++ JWTTokenHandlerScript.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE JWTTokenHandler
+
+USE JWTTokenHandler
+
+CREATE TABLE [dbo].[User](
+[UserID] [int] IDENTITY(1,1) PRIMARY KEY,
+[UserName] [nvarchar](30) NOT NULL,
+[PASSWORD] [nvarchar](30) NOT NULL
+)
+
+
+CREATE TABLE [dbo].[RefreshToken](
+[RefreshTokenID] [int] IDENTITY(1,1) PRIMARY KEY,
+[Token] [nvarchar](30) NOT NULL,
+[UserID] int,
+CONSTRAINT [RefreshToken_User] FOREIGN KEY (UserID)
+REFERENCES [dbo].[User] ([UserID])
+)
